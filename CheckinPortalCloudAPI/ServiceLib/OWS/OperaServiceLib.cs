60c708511b446ae489c35b04f1e28c6941c1e25d
Muhammed K <k.muhammed@sbssolutioninc.com>
2023-06-13 15:25:30 +0400

    Revert "commtopera"
    This reverts commit d5fb7df1cebdd35454f401f294ec77af3d1cb341.

--- CheckinPortalCloudAPI/ServiceLib/OWS/OperaServiceLib.cs
+++ CheckinPortalCloudAPI/ServiceLib/OWS/OperaServiceLib.cs
@@ -4449,40 +4449,25 @@ namespace CheckinPortalCloudAPI.ServiceLib.OWS
 
 
                     List<Models.OWS.GuestMessage> LGM = new List<Models.OWS.GuestMessage>();
-                    if (GMResponse.GuestMessages != null)
+                    foreach (ReservationAdvancedService.GuestMessage GM in GMResponse.GuestMessages)
                     {
-                        foreach (ReservationAdvancedService.GuestMessage GM in GMResponse.GuestMessages)
-                        {
-
-                            Models.OWS.GuestMessage GuestMessages = new Models.OWS.GuestMessage();
-                            GuestMessages.MessageDate = GM.Date;
-                            ReservationAdvancedService.UniqueID UID = GM.GuestMessageID;
-                            GuestMessages.MessageID = UID.Value;
-                            GuestMessages.RecepientName = GM.RecipientName;
-                            GuestMessages.Message = GM.Value;
-                            GuestMessages.MessageStatus = GM.StatusFlag;
-                            LGM.Add(GuestMessages);
-                        }
-
-
-                        return new Models.OWS.OwsResponseModel
-                        {
-                            responseData = LGM,
-                            responseMessage = "Success",
-                            result = true,
-                            statusCode = 101
-                        };
+                        Models.OWS.GuestMessage GuestMessages = new Models.OWS.GuestMessage();
+                        GuestMessages.MessageDate = GM.Date;
+                        ReservationAdvancedService.UniqueID UID = GM.GuestMessageID;
+                        GuestMessages.MessageID = UID.Value;
+                        GuestMessages.RecepientName = GM.RecipientName;
+                        GuestMessages.Message = GM.Value;
+                        GuestMessages.MessageStatus = GM.StatusFlag;
+                        LGM.Add(GuestMessages);
                     }
-                    else
+
+                    return new Models.OWS.OwsResponseModel
                     {
-                        return new Models.OWS.OwsResponseModel
-                        {
-                            responseData = null,
-                            responseMessage = "No guest messages found",
-                            result = false,
-                            statusCode = 102
-                        };
-                    }
+                        responseData = LGM,
+                        responseMessage = "Success",
+                        result = true,
+                        statusCode = 101
+                    };
 
                 }
                 else
@@ -4490,7 +4475,7 @@ namespace CheckinPortalCloudAPI.ServiceLib.OWS
                     return new Models.OWS.OwsResponseModel
                     {
                         responseData = null,
-                        responseMessage = GMResponse.Result != null ? JsonConvert.SerializeObject(GMResponse.Result) : "Failled",
+                        responseMessage = GMResponse.Result != null ? GMResponse.Result.Text[0].Value : "Failled",
                         result = false,
                         statusCode = 102
                     };
@@ -4499,7 +4484,6 @@ namespace CheckinPortalCloudAPI.ServiceLib.OWS
             }
             catch (Exception ex)
             {
-                System.IO.File.WriteAllText(System.Web.Hosting.HostingEnvironment.MapPath(@"~\request1.txt"), ex.ToString());
                 return new Models.OWS.OwsResponseModel
                 {
                     responseData = null,
@@ -4775,7 +4759,6 @@ namespace CheckinPortalCloudAPI.ServiceLib.OWS
             }
             catch (Exception ex)
             {
-             
                 return new Models.OWS.OwsResponseModel
                 {
                     responseData = null,
